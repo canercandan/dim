b347da74070ef914b626034dadebab42bc5c1da1
Caner Candan <candan@info.univ-angers.fr>
2013-02-19 07:52:56 +0100

    * src/dim/evolver/Base.h: created the evolver base class for asynchronious model

--- src/dim/evolver/Base.h
+++ src/dim/evolver/Base.h
@@ -26,15 +26,30 @@ namespace dim
 {
     namespace evolver
     {
-
-	template <typename EOT>
-	class Base : public core::IslandOperator<EOT>
+	namespace sync
 	{
-	public:
-	    virtual void firstCall(core::Pop<EOT>&, core::IslandData<EOT>&) {}
-	    virtual void lastCall(core::Pop<EOT>&, core::IslandData<EOT>&) {}
-	};
+	    template <typename EOT>
+	    class Base : public core::sync::IslandOperator<EOT>
+	    {
+	    public:
+		virtual void firstCall(core::Pop<EOT>&, core::IslandData<EOT>&) {}
+		virtual void lastCall(core::Pop<EOT>&, core::IslandData<EOT>&) {}
+	    };
+	} // !sync
 
+	namespace async
+	{
+	    template <typename EOT>
+	    class Base : public core::async::IslandOperator<EOT>
+	    {
+	    public:
+		virtual void firstCompute(core::Pop<EOT>&, core::IslandData<EOT>&) {}
+		virtual void lastCompute(core::Pop<EOT>&, core::IslandData<EOT>&) {}
+
+		virtual void firstCommunicate(core::Pop<EOT>&, core::IslandData<EOT>&) {}
+		virtual void lastCommunicate(core::Pop<EOT>&, core::IslandData<EOT>&) {}
+	    };
+	} // !async
     } // !evolver
 } // !dim
 
