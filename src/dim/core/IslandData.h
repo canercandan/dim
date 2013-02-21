41d21272405a722f6e2794219e2d02bba40bcce2
Caner Candan <candan@info.univ-angers.fr>
2013-02-21 18:59:52 +0100

    * ../src/dim/core/IslandData.h: created new data structures to share smart queues between threads easily

--- src/dim/core/IslandData.h
+++ src/dim/core/IslandData.h
@@ -32,29 +32,95 @@ namespace dim
 {
     namespace core
     {
+	template <typename T>
+	struct DataQueue
+	{
+	    std::mutex mutex;
+	    std::queue<T> dataQueue;
+	    std::queue< std::chrono::time_point< std::chrono::system_clock > > timesQueue;
+	    std::queue<size_t> idQueue;
+
+	    void push(T newData, size_t id = 0)
+	    {
+		std::lock_guard<std::mutex> lock(mutex);
+		dataQueue.push(newData);
+		timesQueue.push(std::chrono::system_clock::now());
+		idQueue.push(id);
+	    }
+
+	    std::tuple<T, long, size_t> pop()
+	    {
+		std::lock_guard<std::mutex> lock(mutex);
+		auto end = std::chrono::system_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( end - timesQueue.front() ).count();
+		std::tuple<T, long, size_t> ret(dataQueue.front(), elapsed, idQueue.front());
+		dataQueue.pop();
+		timesQueue.pop();
+		idQueue.pop();
+		return ret;
+	    }
+
+	    bool empty()
+	    {
+		std::lock_guard<std::mutex> lock(mutex);
+		return dataQueue.empty();
+	    }
+
+	    size_t size()
+	    {
+		std::lock_guard<std::mutex> lock(mutex);
+		return dataQueue.size();
+	    }
+	};
+
+	template <typename T>
+	struct DataQueueVector : public std::vector< DataQueue< T > >, public ParallelContext
+	{
+	    using ParallelContext::size;
+
+	    DataQueueVector() : std::vector< DataQueue< T > >(size()) {}
+
+	    void push(T newData, size_t id)
+	    {
+		auto& dataQueue = (*this)[id];
+		dataQueue.push(newData, id);
+	    }
+
+	    std::tuple<T, long, size_t> pop(size_t id)
+	    {
+		auto& dataQueue = (*this)[id];
+		return dataQueue.pop();
+	    }
+
+	    bool empty(size_t id) const
+	    {
+		auto& dataQueue = (*this)[id];
+		return dataQueue.empty();
+	    }
+
+	    size_t size(size_t id) const
+	    {
+		auto& dataQueue = (*this)[id];
+		return dataQueue.size();
+	    }
+	};
 
 	template <typename EOT>
 	struct IslandData : public ParallelContext
 	{
 	    using ParallelContext::size;
-
 	    using Fitness = typename EOT::Fitness;
-	    using Mutex = std::mutex;
-	    using Time = std::chrono::time_point<std::chrono::system_clock>;
 
-	    template <typename T> using Vector = std::vector< T >;
-	    template <typename T> using Queue = std::queue< T >;
-	    template <typename T, typename U> using Data = std::tuple< Mutex, T, U >;
-	    template <typename T> using VDQ = Vector< Data< Queue<T>, Queue<Time> > >;
+	    IslandData() : feedbacks(size()), proba(size()) {}
 
-	    Vector<Fitness> feedbacks = Vector<Fitness>(size(), 0);
-	    Vector<Fitness> proba = Vector<Fitness>(size(), 0);
+	    std::vector< Fitness > feedbacks = std::vector< Fitness >(size(), 0);
+	    std::vector< Fitness > proba = std::vector< Fitness >(size(), 0);
 
-	    VDQ<Fitness> feedbackerSendingQueuesVector = VDQ<Fitness>( size() );
-	    VDQ<Fitness> feedbackerReceivingQueuesVector = VDQ<Fitness>( size() );
+	    DataQueueVector< Fitness > feedbackerSendingQueue;
+	    DataQueue< Fitness > feedbackerReceivingQueue;
 
-	    VDQ<EOT> migratorSendingQueuesVector = VDQ<EOT>( size() );
-	    VDQ<EOT> migratorReceivingQueuesVector = VDQ<EOT>( size() );
+	    DataQueueVector< EOT > migratorSendingQueue;
+	    DataQueue< EOT > migratorReceivingQueue;
 	};
 
     } // !core
