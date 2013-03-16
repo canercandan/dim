d04ca755635a79e16c20ca07abb92c8cd49c2fc8
Caner Candan <candan@info.univ-angers.fr>
2013-03-16 16:09:48 +0100

    + measure.py

---
+++ script/measure.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import numpy as np
+import sys
+
+FILES = ['gen', 'evolve', 'feedback', 'update', 'memorize', 'migrate']
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Number of islands missing.")
+        sys.exit()
+
+    print("Time unit: second")
+
+    N = int(sys.argv[1])
+
+    print("%10s " % " ", end=' ')
+    for i in range(N):
+        print("%10d" % i, end=' ')
+    print()
+
+    for f in FILES:
+        print("%10s:" % f, end=' ')
+        for i in range(N):
+            fn = '%s.time.%d' % (f, i)
+            d = open(fn).readline().split()
+            d = [int(x) for x in d]
+            print( '%10.2f' % (np.mean(d)/1000), end=' ' )
+        print()
