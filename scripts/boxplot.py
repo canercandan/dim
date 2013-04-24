ec5b98d823dce2817c175cca90dcb076c311c4ad
Caner Candan <candan@info.univ-angers.fr>
2013-04-24 23:21:15 +0200

    * ../../../scripts/boxplot.py: added more options

--- scripts/boxplot.py
+++ scripts/boxplot.py
@@ -20,65 +20,78 @@
 
 # http://stackoverflow.com/questions/14952401/creating-double-boxplots-i-e-two-boxes-for-each-x-value
 
-import sys, os
 import argparse, logging
-import pylab as pl
-import numpy as np
 
 logger = logging.getLogger("boxplot")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='To trace boxplot.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
+def main():
+    parser = argparse.ArgumentParser(description='To trace boxplot.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--prefix', '-p', help='monitor prefix name', default='result')
-    parser.add_argument('--islands', '-N', help='number of islands', type=int, default=4)
-    parser.add_argument('--times', '-T', help='number of times', type=int, default=10000)
-    parser.add_argument('--runs', '-R', help='number of runs', type=int, default=21)
-
-    # if len(sys.argv) < 5:
-    #     print("Usage:", os.path.basename(sys.argv[0]), "[MONITOR_PREFIX_NAME] [NUMBER_OF_ISLANDS] [NUMBER_OF_TIMES] [NUMBER_OF_RUNS]")
-    #     sys.exit()
-
+    parser.add_argument('--islands', '-n', help='number of islands', type=int, default=4)
+    parser.add_argument('--times', '-t', help='number of times', type=int, default=10000)
+    parser.add_argument('--runs', '-r', help='number of runs', type=int, default=21)
+    parser.add_argument('--column', '-c', help='column to select', type=int, default=2)
+    parser.add_argument('--notplot', '-np', help='disable plotting', action='store_true')
+    parser.add_argument('--space', '-s', help='padding between each boxplot', type=float, default=.15)
+    parser.add_argument('--colors', '-C', help='colors used in order to color each boxplot, use a comma to add more than one color', default='green,black,orange,red,blue,gray,yellow')
     args = parser.parse_args()
-
-    # prefix = sys.argv[1]
-    # Ni = int(sys.argv[2])
-    # Nt = int(sys.argv[3])
-    # Nr = int(sys.argv[4])
 
     V = []
     for i in range(args.islands):
         files = []
-        for r in range(args.runs):
-            fn = "%s_%d_monitor_%d" % (args.prefix, r+1, i)
+
+        if not args.runs:
+            fn = "%s_monitor_%d" % (args.prefix, i)
             print("Pre-opening of run files…", fn)
             f = open(fn)
             files += [(fn, f)]
             f.readline() # to ignore first line
+        else:
+            for r in range(args.runs):
+                fn = "%s_%d_monitor_%d" % (args.prefix, r+1, i)
+                print("Pre-opening of run files…", fn)
+                f = open(fn)
+                files += [(fn, f)]
+                f.readline() # to ignore first line
 
         T = []
         for t in range(args.times):
             R = []
             for r in range(args.runs):
                 fn, f = files[r]
-                nbindi = int(f.readline().split()[2])
+                nbindi = int(f.readline().split()[args.column])
                 print("Reading…", fn, "with time…", t, "and nbindi", nbindi)
                 R += [nbindi]
+
+                print(R)
+
             T += [R]
 
         V += [T]
 
-    colors = ['green', 'black', 'orange', 'red', 'blue', 'gray', 'yellow']
-    space = .15
-    pos = np.linspace(args.islands/2*space, -args.islands/2*space, args.islands).T
+    print(V)
 
-    for i in range(args.islands):
-        r = pl.boxplot(V[i], positions=[x-pos[i] for x in range(args.times)], widths=0.1)
-        for value in r.values(): pl.setp(value, color=colors[i%len(colors)])
+    if not args.notplot:
+        import pylab as pl
+        import numpy as np
 
-    pl.legend(["isl %s (%s)" % (i, colors[i]) for i in range(args.islands)])
-    pl.xlabel('Time')
-    pl.ylabel('Nb individuals')
-    pl.xticks([x for x in range(args.times)])
-    pl.xlim(-args.islands/2*space*2, (args.times-1)+args.islands/2*space*2)
-    pl.show()
+        colors = args.colors.split(',')
+        pos = np.linspace(args.islands/2*args.space, -args.islands/2*args.space, args.islands).T
+
+        for i in range(args.islands):
+            r = pl.boxplot(V[i], positions=[x-pos[i] for x in range(args.times)], widths=0.1)
+            for value in r.values(): pl.setp(value, color=colors[i%len(colors)])
+
+        pl.legend(["isl %s (%s)" % (i, colors[i]) for i in range(args.islands)])
+        pl.xlabel('Time')
+        pl.ylabel('Nb individuals')
+        pl.xticks([x for x in range(args.times)])
+        pl.xlim(-args.islands/2*args.space*2, (args.times-1)+args.islands/2*args.space*2)
+        pl.show()
+
+# when executed, just run main():
+if __name__ == '__main__':
+    logger.debug('### script started ###')
+    main()
+    logger.debug('### script ended ###')
