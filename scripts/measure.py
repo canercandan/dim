4dac5c70352f4be72fbefbc44dc2a6e9f4d401b4
Caner Candan <candan@info.univ-angers.fr>
2013-05-15 17:07:18 +0200

    * ../../../scripts/measure.py: parsing first then printing

--- scripts/measure.py
+++ scripts/measure.py
@@ -22,23 +22,27 @@ import logging
 from parser import Parser
 import numpy as np
 from collections import OrderedDict
+import sys
 
 logger = logging.getLogger("measure")
 timeunits = OrderedDict([('seconds', 10**0), ('milliseconds', 10**3), ('microseconds', 10**6), ('nanoseconds', 10**9),])
 
-def main():
-    parser = Parser(description='To measure execution time for each part of the algorithm of DIM.')
-    parser.add_argument('--islands', '-n', help='number of islands', type=int, default=4)
-    parser.add_argument('--files', '-f', help='list of files prefixes to display, separated by comma', default='gen,evolve,feedback,update,memorize,migrate')
-    parser.add_argument('--time', '-t', choices=[x for x in timeunits.keys()], help='select a time unit', default='seconds')
-    parser.add_argument('--seconds', '-s', action='store_const', const='seconds', dest='time', help='time in seconds')
-    parser.add_argument('--milliseconds', '-m', action='store_const', const='milliseconds', dest='time', help='time in milliseconds')
-    parser.add_argument('--microseconds', '-u', action='store_const', const='microseconds', dest='time', help='time in microseconds')
-    parser.add_argument('--nanoseconds', '-N', action='store_const', const='nanoseconds', dest='time', help='time in nanoseconds')
-    args = parser()
+def parse_data(args):
+    data = [None]*args.islands
+    try:
+        for i in range(args.islands):
+            island = {}
+            for f in args.files:
+                fn = '%s%s%d' % (f, args.suffix, i)
+                d = [int(x) for x in open(fn).readline().split()]
+                island[f] = np.mean(d)*timeunits[args.time]/1000
+            data[i] = island
+    except FileNotFoundError as e:
+        logger.error('File not found: %s' % e)
+        sys.exit()
+    return data
 
-    logger.debug(args)
-
+def print_data(args, data):
     print("Time unit: %s" % args.time)
 
     print("%10s " % " ", end=' ')
@@ -46,14 +50,29 @@ def main():
         print("%10d" % i, end=' ')
     print()
 
-    for f in args.files.split(','):
+    for f in args.files:
         print("%10s:" % f, end=' ')
         for i in range(args.islands):
-            fn = '%s.time.%d' % (f, i)
-            d = open(fn).readline().split()
-            d = [int(x) for x in d]
-            print( '%10.2f' % (np.mean(d)*timeunits[args.time]/1000), end=' ' )
+            print( '%10.2f' % data[i][f], end=' ' )
         print()
+
+def main():
+    parser = Parser(description='To measure execution time for each part of the algorithm of DIM.', verbose='error')
+    parser.add_argument('--islands', '-n', help='number of islands', type=int, default=4)
+    parser.add_argument('--files', '-f', help='list of files prefixes to display, separated by comma', default='gen,evolve,feedback,update,memorize,migrate')
+    parser.add_argument('--suffix', help='set the suffix time files', default='.time.')
+    parser.add_argument('--time', '-t', choices=[x for x in timeunits.keys()], help='select a time unit', default='milliseconds')
+    parser.add_argument('--seconds', '-s', action='store_const', const='seconds', dest='time', help='time in seconds')
+    parser.add_argument('--milliseconds', '-m', action='store_const', const='milliseconds', dest='time', help='time in milliseconds')
+    parser.add_argument('--microseconds', '-u', action='store_const', const='microseconds', dest='time', help='time in microseconds')
+    parser.add_argument('--nanoseconds', '-N', action='store_const', const='nanoseconds', dest='time', help='time in nanoseconds')
+    parser.add_argument('--percent', '-p', action='store_true', help='use percents')
+    args = parser()
+
+    args.files = args.files.split(',')
+
+    data = parse_data(args)
+    print_data(args, data)
 
 # when executed, just run main():
 if __name__ == '__main__':
