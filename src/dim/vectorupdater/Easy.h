bc7c9966a0adbcf78612b8226cba2175e7fbdb5e
Caner Candan <candan@info.univ-angers.fr>
2013-05-22 13:58:26 +0200

    * ../../../src/dim/vectorupdater/Easy.h: removed deprecated code lines

--- src/dim/vectorupdater/Easy.h
+++ src/dim/vectorupdater/Easy.h
@@ -86,13 +86,7 @@ namespace dim
 		for (size_t i = 0; i < this->size(); ++i)
 		    {
 			R[i] = T[i] >= tau ? S[i] : 0;
-// #ifdef TRACE
-// 			_of << R[i] << " ";
-// #endif //! TRACE
 		    }
-// #ifdef TRACE
-// 		_of << std::endl; std::cout.flush();
-// #endif //! TRACE
 
 		// Stategie par critère MAX
 		// int best = -1;
@@ -109,64 +103,29 @@ namespace dim
 		// Stratégie par récompense proportionnelle
 		{
 		    typename EOT::Fitness sum_fits = std::accumulate(R.begin(), R.end(), 0., bounded_sum );
-
-// #ifdef TRACE
-// 		    _of << sum_fits << " "; _of.flush();
-// #endif //! TRACE
-
 		    if (sum_fits)
 			{
-
 			    unsigned sum_sum = 0;
-
 			    for (size_t i = 0; i < R.size()-1; ++i)
 				{
 				    R[i] = R[i] > 0 ? R[i] / sum_fits * 1000 : 0;
 				    sum_sum += R[i];
-// #ifdef TRACE
-// 			    _of << R[i] << " ";
-// #endif //! TRACE
 				}
-
 			    R.back() = 1000-sum_sum;
-// #ifdef TRACE
-// 		    _of << R.back();
-// 		    _of << std::endl; std::cout.flush();
-// #endif
-
 			}
 		    else
 			{
 			    tau = std_or_boost::chrono::system_clock::now();
-
 			    eo::log << eo::warnings << "S is null" << std::endl; eo::log.flush();
-
 			    return;
-
-			    // unsigned sum = 0;
-
-			    // for ( size_t i = 0; i < this->size()-1; ++i )
-			    // 	{
-			    // 	    R[i] = 1000 / this->size();
-			    // 	    sum += R[i];
-			    // 	}
-
-			    // R.back() = 1000-sum;
 			}
 		}
-
-		// _of << std::accumulate(R.begin(), R.end(), 0., bounded_sum ) << " ";
-
-#ifdef TRACE
-		_of << R.back() << " ";
-#endif //! TRACE
 
 		// computation of epsilon vector (norm is 1)
 		std::vector< double > epsilon( this->size() );
 
 		{
 		    unsigned sum = 0;
-
 		    for (size_t i = 0; i < epsilon.size(); ++i)
 			{
 			    epsilon[i] = rng.rand() % 1000;
@@ -174,13 +133,11 @@ namespace dim
 			}
 
 		    unsigned sum_sum = 0;
-
 		    for (size_t i = 0; i < epsilon.size()-1; ++i)
 			{
 			    epsilon[i] = sum ? epsilon[i] / sum * 1000 : 0;
 			    sum_sum += epsilon[i];
 			}
-
 		    epsilon.back() = 1000-sum_sum;
 		}
 
@@ -201,10 +158,6 @@ namespace dim
 
 		AUTO(double) elapsed = std_or_boost::chrono::duration_cast<std_or_boost::chrono::microseconds>( std_or_boost::chrono::system_clock::now() - tau ).count() / 1000.; // \DELTA{t}
 
-// #ifdef TRACE
-// 		_of << elapsed << " "; _of.flush();
-// #endif // !TRACE
-
 		if (!_delta)
 		    {
 			elapsed = 1.;
@@ -212,54 +165,13 @@ namespace dim
 
 		AUTO(double) alphaT = _alpha ? exp(log(_alpha /*0.2*/)/(elapsed*_sensitivity)) : 0;
 		AUTO(double) betaT = _beta ? exp(log(_beta /*0.01*/)/(elapsed*_sensitivity)) : 0;
-		// AUTO(double) betaT = _beta;
-
-		// std::cout << betaT << " "; std::cout.flush();
-
-#ifdef TRACE
-		_of << alphaT << " " << betaT << " "; _of.flush();
-#endif // !TRACE
-		// {
-		//     unsigned sum = 0;
-
-		//     for ( size_t i = 0; i < this->size()-1; ++i )
-		// 	{
-		// 	    R[i] = 1000 / this->size();
-		// 	    sum += R[i];
-		// 	}
-
-		//     R.back() = 1000-sum;
-		// }
-
-		// std::cout << std::accumulate(R.begin(), R.end(), 0.) << " "; std::cout.flush();
 
 		unsigned sum = 0;
-
 		for ( size_t i = 0; i < this->size()-1; ++i )
 		    {
-			// typename EOT::Fitness Ri = R[i] / sum_multi;
-
-// #ifdef TRACE
-// 			_of << "(" << i << ") "
-// 			    << (1 - betaT) << " * ( "
-// 			    << 1 - alphaT << " * "
-// 			    << data.proba[i] << " + "
-// 			    << alphaT << " * "
-// 			    << R[i] << " )"
-// 			    << " + " << betaT << " * " << 1000 << " * " << epsilon[i]
-// 			    << " = ";
-// #endif // !TRACE
-
 			data.proba[i] = (1 - betaT) * ( ( 1 - alphaT ) * data.proba[i] + alphaT * R[i] ) + betaT * epsilon[i];
 			sum += data.proba[i];
-
-// #ifdef TRACE
-// 			_of << data.proba[i] << std::endl;
-// 			_of.flush();
-// #endif // !TRACE
-
 		    }
-
 		data.proba.back() = 1000-sum;
 
 		tau = std_or_boost::chrono::system_clock::now();
@@ -278,7 +190,6 @@ namespace dim
 #ifdef TRACE
 	    std::ofstream _of;
 #endif // !TRACE
-
 	};
     } // !vectorupdater
 } // !dim
