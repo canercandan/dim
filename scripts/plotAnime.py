67c4ba3d1d07ee2f83c280f9099403bd960592d8
Caner Candan <candan@info.univ-angers.fr>
2013-06-05 07:53:21 +0200

    * ../../../scripts/plotAnime.py: removed useless options and added an option to select the stats we want to display

--- scripts/plotAnime.py
+++ scripts/plotAnime.py
@@ -28,14 +28,12 @@ import time
 
 logger = logging.getLogger("plotAnim")
 
-topics = ['attractiveness_sumrank', 'attractiveness', 'field_sumrank', 'field']
-
 class DataParser:
     def __init__(self, args):
         self.args = args
         self.rm = results_mixer.ResultsMixer(**vars(args))
         self.lastValues = {}
-        for t in topics:
+        for t in self.args.topics:
             self.lastValues[t] = [0]*self.args.islands
 
     def create_sumrank(self, data, key, values, record):
@@ -62,13 +60,21 @@ class DataParser:
                 for j in range(self.args.islands):
                     inputProbas += [float(infos[j]['probas'][i])]
                 tmp += [sum(inputProbas)]
-            self.create_sumrank(selectedData, 'attractiveness_sumrank', tmp, record)
-            record['attractiveness'] = tmp
+
+            if 'attractiveness_sumrank' in self.args.topics:
+                self.create_sumrank(selectedData, 'attractiveness_sumrank', tmp, record)
+
+            if 'attractiveness' in self.args.topics:
+                record['attractiveness'] = tmp
 
             # field
             values = [float(info[self.args.field]) for info in infos]
-            self.create_sumrank(selectedData, 'field_sumrank', values, record)
-            record['field'] = values
+
+            if 'field_sumrank' in self.args.topics:
+                self.create_sumrank(selectedData, 'field_sumrank', values, record)
+
+            if 'field' in self.args.topics:
+                record['field'] = values
 
             selectedData += [record]
 
@@ -86,8 +92,12 @@ class DataUpdater:
         self.axes = {}
         self.lines = {}
 
-        for k, t in enumerate(topics):
-            ax = self.fig.add_subplot(2,len(topics)/2,k+1)
+        for k, t in enumerate(self.args.topics):
+            ax = None
+            if len(self.args.topics) == 1:
+                ax = self.fig.add_subplot(111)
+            else:
+                ax = self.fig.add_subplot(2,len(self.args.topics)/2,k+1)
 
             line = []
             for i in range(args.islands):
@@ -104,7 +114,7 @@ class DataUpdater:
             self.axes[t] = ax
             self.lines[t] = line
 
-        self.fig.legend(self.lines[topics[0]], loc='upper left', labels=['island %d' % i for i in range(args.islands)])
+        self.fig.legend(self.lines[self.args.topics[0]], loc='upper left', labels=['island %d' % i for i in range(args.islands)])
 
         self.dp = DataParser(args)
         self.anim = animation.FuncAnimation(self.fig, self, self.dp, interval=args.interval)
@@ -114,7 +124,7 @@ class DataUpdater:
     def __call__(self, data):
         if not data: return
 
-        for t in topics:
+        for t in self.args.topics:
             for i in range(self.args.islands):
                 d = [ l[t][i] for l in data[::self.args.affinity] ]
 
@@ -131,28 +141,23 @@ def main():
     parser = Parser(description='To trace data.')
     parser.add_argument('--islands', '-n', help='number of islands', type=int, default=4)
     parser.add_argument('--field', '-f', help='field to trace', default='nbindi')
-    parser.add_argument('--attractiveness', action='store_true', help='instead tracing a field, trace the attractiveness thanks to coming probability')
-    parser.add_argument('--sumrank', '-r', action='store_true', help='trace the cumulate of sum of times the islands lead')
     parser.add_argument('--xlabel', help='label for x-axe', default='time (in seconds)')
-    parser.add_argument('--ylabel', help='label for y-axe', default='number of individuals in waiting')
-    parser.add_argument('--ylabelAttractiveness', help='label for y-axe if -a is enabled', default='attractiveness')
-    parser.add_argument('--ylabelSumrank', help='prefix label for y-axe', default='sum of rank along with the')
+    parser.add_argument('--ylabel', help='label for y-axe', default='y')
     parser.add_argument('--affinity', '-a', help='plot the defined file', type=int, default=1)
-    parser.add_argument('--outputFile', '-O', help='output file where the figure will be saved', default='output.png')
-    parser.add_argument('--show', '-s', action='store_true', help='plot instead creating a new file')
-    parser.add_argument('--smoothing', '-S', type=int, help='smooth the data set, give a level of smoothing (0 = no smoothing)', default=0)
-    parser.add_argument('--no-marker', action='store_false', help='dont trace markers')
     parser.add_argument('--no-grid', action='store_false', help='dont trace grid')
-    parser.add_argument('--animate', '-A', help='animate measures', action='store_true')
     parser.add_argument('--scale', type=int, default=100, help='scale of animation view (0 means dynamic)')
     parser.add_argument('--yscale', type=int, default=50, help='scale of animation view for y-axe')
     parser.add_argument('--interval', type=int, default=100, help='interval data of animation view')
     parser.add_argument('--timeunit', '-t', choices=[x for x in results_mixer.timeunits.keys()], help='select a time unit', default='seconds')
     parser.add_argument('--timeinterval', '-I', type=int, help='interval of times between two records based on the selected time unit (see -t)', default=1)
     parser.add_argument('--respawn', '-R', action='store_true', help='plot instead creating a new file')
+    parser.add_argument('--topics', '-T', help='select stats to display', default='attractiveness_sumrank,attractiveness,field_sumrank,field')
 
     args = parser()
     logger.debug(args)
+
+    args.topics = args.topics.split(',')
+
     du = DataUpdater(args)
 
 # when executed, just run main():
