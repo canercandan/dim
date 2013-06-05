ff4d342f4c406c6af295aa5047508c780cc6c74e
Caner Candan <candan@info.univ-angers.fr>
2013-06-05 07:49:33 +0200

    * ../../../src/dim/vectorupdater/Easy.h: added Rewarding operator to split best from avg strategy

--- src/dim/vectorupdater/Easy.h
+++ src/dim/vectorupdater/Easy.h
@@ -53,73 +53,37 @@ namespace dim
 #endif
 
 	template <typename EOT>
-	class Easy : public Base<EOT>
+	class Reward : public core::IslandOperator<EOT>
 	{
 	public:
-	    Easy( double alpha = 0.2 /*1-0.8*/, double beta = 0.01 /*1-0.99*/, double sensitivity = 1., bool delta = true )
-		: _alpha(alpha), _beta(beta), _sensitivity(sensitivity), _delta(delta)
-	    {
-#ifdef TRACE
-		std::ostringstream ss;
-		ss << "trace.updater." << this->rank();
-		_of.open(ss.str().c_str());
-#endif // !TRACE
-	    }
+	    virtual void firstCall(core::Pop<EOT>&, core::IslandData<EOT>&) {}
+	    virtual void lastCall(core::Pop<EOT>&, core::IslandData<EOT>&) {}
+	};
 
-	    static inline typename EOT::Fitness bounded_sum(typename EOT::Fitness x, typename EOT::Fitness y){ return std::max(x, typename EOT::Fitness(0)) + std::max(y, typename EOT::Fitness(0)); }
+	template <typename EOT>
+	class Best : public Reward<EOT>
+	{
+	public:
+	    Best( double alpha = 0.2 /*1-0.8*/, double beta = 0.01 /*1-0.99*/ )
+		: _alpha(alpha), _beta(beta)
+	    {}
+
+	    virtual ~Best() {}
 
 	    void operator()(core::Pop<EOT>& pop, core::IslandData<EOT>& data)
 	    {
-		if (pop.empty()) { return; }
-
-		/****************************
-		 * Update transition vector *
-		 ****************************/
-
-		// Prepare the vector R
-		// Ri = { Si, if Ti > \tau; 0, otherwise }
 		AUTO(typename BOOST_IDENTITY_TYPE((std::vector< typename EOT::Fitness >)))& S = data.feedbacks;
-		AUTO(typename BOOST_IDENTITY_TYPE((std::vector< std_or_boost::chrono::time_point< std_or_boost::chrono::system_clock > >)))& T = data.feedbackLastUpdatedTimes;
-		AUTO(typename BOOST_IDENTITY_TYPE((std_or_boost::chrono::time_point< std_or_boost::chrono::system_clock >)))& tau = data.vectorLastUpdatedTime;
 
-		std::vector< typename EOT::Fitness > R( this->size() );
+		int best = -1;
+		typename EOT::Fitness max = -1;
 		for (size_t i = 0; i < this->size(); ++i)
 		    {
-			R[i] = T[i] >= tau ? S[i] : 0;
+			if (S[i] > max)
+			    {
+				best = i;
+				max = S[i];
+			    }
 		    }
-
-		// Stategie par critère MAX
-		// int best = -1;
-		// typename EOT::Fitness max = 0;
-		// for (size_t i = 0; i < this->size(); ++i)
-		// 	{
-		// 	    if (data.feedbacks[i] > max)
-		// 		{
-		// 		    best = i;
-		// 		    max = data.feedbacks[i];
-		// 		}
-		// 	}
-
-		// Stratégie par récompense proportionnelle
-		{
-		    typename EOT::Fitness sum_fits = std::accumulate(R.begin(), R.end(), 0., bounded_sum );
-		    if (sum_fits)
-			{
-			    unsigned sum_sum = 0;
-			    for (size_t i = 0; i < R.size()-1; ++i)
-				{
-				    R[i] = R[i] > 0 ? R[i] / sum_fits * 1000 : 0;
-				    sum_sum += R[i];
-				}
-			    R.back() = 1000-sum_sum;
-			}
-		    else
-			{
-			    tau = std_or_boost::chrono::system_clock::now();
-			    eo::log << eo::warnings << "S is null" << std::endl; eo::log.flush();
-			    return;
-			}
-		}
 
 		// computation of epsilon vector (norm is 1)
 		std::vector< double > epsilon( this->size() );
@@ -141,20 +105,100 @@ namespace dim
 		    epsilon.back() = 1000-sum_sum;
 		}
 
-		/*******************************************************************************
-		 * Si p_i^t est le vecteur de migration de ile numéro i au temps t             *
-		 * alors on peut faire un update "baysien" comme ça:                           *
-		 *                                                                             *
-		 * p_i^{t+1} =  b  *  ( a * p_i^t + (1 - a) * select )  +  (1 - b) * epsilon   *
-		 *                                                                             *
-		 * où:                                                                         *
-		 * - a et b sont les coefficients qui reglent respectivement l'exploitation et *
-		 * l'exploration                                                               *
-		 * - select est le vecteur avec que des 0 sauf pour l'ile qui semble la plus   *
-		 * prometteuse où cela vaut 1.                                                 *
-		 * - epsilon un vecteur de 'bruit' (de norme 1) avec des coefficient           *
-		 * uniforme entre 0.0 et 1.0                                                   *
-		 *******************************************************************************/
+		unsigned sum = 0;
+		for ( size_t i = 0; i < this->size()-1; ++i )
+		    {
+			if (best == -1) // no improvment then rebalancing
+			    {
+				data.proba[i] = (1-_beta)*data.proba[i] + _beta*epsilon[i];
+			    }
+			else
+			    {
+				if (i == best)
+				    {
+					data.proba[i] = (1-_beta)*((1-_alpha)*data.proba[i] + _alpha*1000) + _beta*epsilon[i];
+				    }
+				else
+				    {
+					data.proba[i] = (1-_beta)*((1-_alpha)*data.proba[i]) + _beta*epsilon[i];
+				    }
+			    }
+
+			sum += data.proba[i];
+		    }
+		data.proba.back() = 1000-sum;
+	    }
+
+	private:
+	    double _alpha;
+	    double _beta;
+	};
+
+	template <typename EOT>
+	class Proportional : public Reward<EOT>
+	{
+	public:
+	    Proportional( double alpha = 0.2 /*1-0.8*/, double beta = 0.01 /*1-0.99*/, double sensitivity = 1., bool delta = true )
+		: _alpha(alpha), _beta(beta), _sensitivity(sensitivity), _delta(delta)
+	    {}
+
+	    virtual ~Proportional() {}
+
+	    static inline typename EOT::Fitness bounded_sum(typename EOT::Fitness x,
+							    typename EOT::Fitness y)
+	    {
+		return std::max(x, typename EOT::Fitness(0)) + std::max(y, typename EOT::Fitness(0));
+	    }
+
+	    void operator()(core::Pop<EOT>& pop, core::IslandData<EOT>& data)
+	    {
+		AUTO(typename BOOST_IDENTITY_TYPE((std::vector< typename EOT::Fitness >)))& S = data.feedbacks;
+		AUTO(typename BOOST_IDENTITY_TYPE((std::vector< std_or_boost::chrono::time_point< std_or_boost::chrono::system_clock > >)))& T = data.feedbackLastUpdatedTimes;
+		AUTO(typename BOOST_IDENTITY_TYPE((std_or_boost::chrono::time_point< std_or_boost::chrono::system_clock >)))& tau = data.vectorLastUpdatedTime;
+
+		std::vector< typename EOT::Fitness > R( this->size() );
+		for (size_t i = 0; i < this->size(); ++i)
+		    {
+			R[i] = T[i] >= tau ? S[i] : 0;
+		    }
+
+		typename EOT::Fitness sum_fits = std::accumulate(R.begin(), R.end(), 0., bounded_sum );
+		if (sum_fits)
+		    {
+			unsigned sum_sum = 0;
+			for (size_t i = 0; i < R.size()-1; ++i)
+			    {
+				R[i] = R[i] > 0 ? R[i] / sum_fits * 1000 : 0;
+				sum_sum += R[i];
+			    }
+			R.back() = 1000-sum_sum;
+		    }
+		else
+		    {
+			tau = std_or_boost::chrono::system_clock::now();
+			eo::log << eo::warnings << "S is null" << std::endl; eo::log.flush();
+			return;
+		    }
+
+		// computation of epsilon vector (norm is 1)
+		std::vector< double > epsilon( this->size() );
+
+		{
+		    unsigned sum = 0;
+		    for (size_t i = 0; i < epsilon.size(); ++i)
+			{
+			    epsilon[i] = rng.rand() % 1000;
+			    sum += epsilon[i];
+			}
+
+		    unsigned sum_sum = 0;
+		    for (size_t i = 0; i < epsilon.size()-1; ++i)
+			{
+			    epsilon[i] = sum ? epsilon[i] / sum * 1000 : 0;
+			    sum_sum += epsilon[i];
+			}
+		    epsilon.back() = 1000-sum_sum;
+		}
 
 		AUTO(double) elapsed = std_or_boost::chrono::duration_cast<std_or_boost::chrono::microseconds>( std_or_boost::chrono::system_clock::now() - tau ).count() / 1000.; // \DELTA{t}
 
@@ -170,15 +214,13 @@ namespace dim
 		for ( size_t i = 0; i < this->size()-1; ++i )
 		    {
 			data.proba[i] = (1 - betaT) * ( ( 1 - alphaT ) * data.proba[i] + alphaT * R[i] ) + betaT * epsilon[i];
+
 			sum += data.proba[i];
 		    }
+
 		data.proba.back() = 1000-sum;
 
 		tau = std_or_boost::chrono::system_clock::now();
-
-#ifdef TRACE
-		_of << std::endl; _of.flush();
-#endif // !TRACE
 	    }
 
 	private:
@@ -186,6 +228,29 @@ namespace dim
 	    double _beta;
 	    bool _delta;
 	    double _sensitivity;
+	};
+
+	template <typename EOT>
+	class Easy : public Base<EOT>
+	{
+	public:
+	    Easy( Reward<EOT>& reward )
+		: _reward(reward)
+	    {
+#ifdef TRACE
+		std::ostringstream ss;
+		ss << "trace.updater." << this->rank();
+		_of.open(ss.str().c_str());
+#endif // !TRACE
+	    }
+
+	    void operator()(core::Pop<EOT>& pop, core::IslandData<EOT>& data)
+	    {
+		_reward(pop, data);
+	    }
+
+	private:
+	    Reward<EOT>& _reward;
 
 #ifdef TRACE
 	    std::ofstream _of;
